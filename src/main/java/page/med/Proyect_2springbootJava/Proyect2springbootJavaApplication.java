ecb15ce75fd0d27b81c535c704576d776973466f
Santiago Colorado <130319819+Dasacoga@users.noreply.github.com>
2023-07-20 22:24:01 -0500

    primer commit proyecto2

---
+++ src/main/java/page/med/Proyect_2springbootJava/Proyect2springbootJavaApplication.java
@@ -0,0 +1,13 @@
+package page.med.Proyect_2springbootJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyect2springbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyect2springbootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/page/med/Proyect_2springbootJava/Proyect2springbootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package page.med.Proyect_2springbootJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyect2springbootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
